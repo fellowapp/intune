7c55b0e13a780b44c882900a72436496e67cb76a
macdja38 <jakeincanada@icloud.com>
2024-02-07 13:39:32 -0500

    Rename iOS side of plugin

--- ios/Plugin/IntunePlugin.m
+++ ios/Plugin/IntunePlugin.m
@@ -3,7 +3,7 @@
 
 // Define the plugin using the CAP_PLUGIN Macro, and
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
-CAP_PLUGIN(IntunePlugin, "Intune",
+CAP_PLUGIN(IntunePlugin, "@fellow/Intune",
            CAP_PLUGIN_METHOD(loginAndEnrollAccount, CAPPluginReturnNone);
            CAP_PLUGIN_METHOD(registerAndEnrollAccount, CAPPluginReturnNone);
            CAP_PLUGIN_METHOD(deRegisterAndUnenrollAccount, CAPPluginReturnNone);
